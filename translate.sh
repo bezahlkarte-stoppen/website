6dfe202c703f55c9b757ce2b724241bbdea21cad
Falco Nordmann <6012856+68b32@users.noreply.github.com>
2025-07-08 12:47:05 +0200

    Change pathes

--- translate.sh
+++ translate.sh
@@ -17,12 +17,12 @@ cd "$SCRIPT_DIR" || exit
 for l in en tr uk fr es ru; do
 	rm -rf www/$l
 	mkdir www/$l
-	$DEEPL_CLI doc -O html -f de -t $l -o ../www/$l/index.html ../www/de/index.html
+	$DEEPL_CLI doc -O html -f de -t $l -o www/$l/index.html www/de/index.html
 done
 
 # ChatGPT
 for l in ar fa; do
 	rm -rf www/$l
 	mkdir www/$l
-	$CHATGPT_CLI --track-token-usage=false --model=gpt-4o --role-file ./translate.prompt -p ../www/de/index.html --query "Zielspache: $l" --max-tokens 16384 --context-window 24000  > ../www/$l/index.html
+	$CHATGPT_CLI --track-token-usage=false --model=gpt-4o --role-file ./translate.prompt -p www/de/index.html --query "Zielspache: $l" --max-tokens 16384 --context-window 24000  > www/$l/index.html
 done
