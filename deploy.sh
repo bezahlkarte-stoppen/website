d78edb36e68f84c7ba67ed4c0015036e99c18caa
Falco Nordmann <6012856+68b32@users.noreply.github.com>
2025-07-09 11:34:24 +0200

    Modified deploy process

---
+++ deploy.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR" || exit
+
+PIDFILE="deploy.pid"
+
+if [ -f "$PIDFILE" ]; then
+	MYPID=$(cat "$PIDFILE")
+
+	if ps -p $MYPID > /dev/null 2>&1; then
+		echo "Another instance of this process is running."
+		exit 1
+	fi
+	rm -f "$PIDFILE"
+fi
+
+echo $$ > "$PIDFILE"
+
+
+PUSH="/home/www/www011/chroot/data-www011/push";
+
+if [ -f "$PUSH" ]; then
+	BRANCH="`cat $PUSH`"
+	if [ "$BRANCH" == "main" ]; then
+
+		exec 3>&1 4>&2
+		trap 'exec 2>&4 1>&3' 0 1 2 3
+		exec 1> >(tee deploy.log) 2>&1
+
+		echo "RUN $0"
+		echo "PWD `pwd`"
+
+		rm -rf website/
+		git clone https://github.com/bezahlkarte-stoppen/website.git website
+		source .env
+		./translate.sh
+		if [ $? -ne 0 ]; then
+			echo "Translation failed. Skipping deployment.";
+			exit 1
+		fi
+
+		echo "Deploy new website..."
+		cp -r ../chroot/data-www011 bak/$(date +%s)
+		rm -rf ../chroot/data-www011/*
+		cp -r website/www/* ../chroot/data-www011
+		cp webhook.php ../chroot/data-www011
+		echo "Deploy done.";
+	fi
+fi
+rm -f "$PIDFILE"
--- translate.sh
+++ translate.sh
@@ -11,19 +11,69 @@
 # es español
 # ru русский язык
 
+TRIES=3
+
+echo "RUN $0"
+echo "PWD `pwd`"
+echo "ENV \$CHATGPT_CLI=$CHATGPT_CLI"
+echo "ENV \$DEEPL_CLI=$CHATGPT_CLI"
+echo "TRIES $TRIES"
+
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit
 
+
 # Deepl
+error=false
 for l in en tr uk fr es ru; do
-	rm -rf www/$l
-	mkdir www/$l
-	$DEEPL_CLI doc -O html -f de -t $l -o www/$l/index.html www/de/index.html
+	dst="website/www/$l/index.html"
+	for ((i = 1; i <= $TRIES; i++)); do
+		echo "[Try $i / $TRIES] Start deepl translation for $l ..."
+		rm -rf website/www/$l
+		mkdir website/www/$l
+		$DEEPL_CLI doc -O html -f de -t $l -o $dst website/www/de/index.html
+		if [ $? -eq 0 ]; then
+			if [ -e "$dst"  -a -s "$dst" ]; then
+				echo "Done deepl translation for $l"
+				break;
+			else
+				echo "ERROR: Translated file $dst does not exist or is empty."
+				error=true
+			fi
+		else
+			echo "ERROR: DEEPL_CLI exited with error."
+			error=true
+		fi
+	done
+
+	$error && echo "ERROR: No success after $TRIES tries" && exit 1
 done
 
+
 # ChatGPT
+error=false
 for l in ar fa; do
-	rm -rf www/$l
-	mkdir www/$l
-	$CHATGPT_CLI --track-token-usage=false --model=gpt-4o --role-file ./translate.prompt -p www/de/index.html --query "Zielspache: $l" --max-tokens 16384 --context-window 24000  > www/$l/index.html
+	dst="website/www/$l/index.html"
+	for ((i = 1; i <= $TRIES; i++)); do
+		echo "[Try $i / $TRIES] Start ChatGPT translation for $l ..."
+		rm -rf website/www/$l
+		mkdir website/www/$l
+		$CHATGPT_CLI --track-token-usage=false --model=gpt-4o --role-file ./translate.prompt -p website/www/de/index.html --query "Zielspache: $l" --max-tokens 16384 --context-window 24000  > $dst
+		if [ $? -eq 0 ]; then
+			if [ -e "$dst"  -a -s "$dst" ]; then
+				echo "Done ChatGPT tanslation for $l"
+				break;
+			else
+				echo "ERROR: Translated file $dst does not exist or is empty."
+				error=true
+			fi
+		else
+			echo "ERROR: CHATGPT_CLI exited with error."
+			error=true
+		fi
+	done
+
+	$error && echo "ERROR: No success after $TRIES tries" && exit 1
 done
+exit 0
